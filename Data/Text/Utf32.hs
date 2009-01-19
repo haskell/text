5bebce75f77841061b7717bf2464593dab819275
Bryan O'Sullivan <bos@serpentine.com>
2009-01-19 12:25:27 +0000

    Tidy up the Utf32 module
    --HG--
    extra : convert_revision : e1a3ac31de60459c7a03acb58760bf1257ee2bfa

--- Data/Text/Utf32.hs
+++ Data/Text/Utf32.hs
@@ -1,6 +1,10 @@
-module Data.Text.Utf32 where
+module Data.Text.Utf32
+    (
+      validate
+    ) where
 
 import Data.Word (Word32)
 
 validate    :: Word32 -> Bool
 validate x1 = (x1 >= 0x0 && x1 < 0xD800) || (x1 > 0xDFFF && x1 <= 0x10FFFF)
+{-# INLINE validate #-}
