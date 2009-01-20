fa8e827e4d11c37c20e14cf9ba05cb1f1aa2c04f
Bryan O'Sullivan <bos@serpentine.com>
2009-01-20 07:44:32 +0000

    Derive Typeable
    --HG--
    extra : convert_revision : 9bc74b29e181bb9deec208d4a40cef4e391ba301

--- Data/Text/Internal.hs
+++ Data/Text/Internal.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Data.Text.Internal
     (
       Text(..)
@@ -6,9 +8,13 @@ module Data.Text.Internal
 
 import Data.Array.ST (newArray_,runSTUArray)
 import Data.Array.Unboxed (UArray)
+import Data.Typeable (Typeable)
 import Data.Word (Word16)
 
-data Text = Text !(UArray Int Word16) {-# UNPACK #-}!Int {-# UNPACK #-}!Int
+data Text = Text {-# UNPACK #-} !(UArray Int Word16) -- payload
+                 {-# UNPACK #-} !Int                 -- offset
+                 {-# UNPACK #-} !Int                 -- length
+            deriving (Typeable)
 
 empty :: Text
 empty = Text (runSTUArray (newArray_ (0,0))) 0 0
