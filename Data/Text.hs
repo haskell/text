cf427a519c4c6ddd017403c9ed85fb3f6c43a126
Bryan O'Sullivan <bos@serpentine.com>
2009-01-21 06:05:29 +0000

    Fix definition of length: forgot surrogate pairs
    --HG--
    extra : convert_revision : e097713823e472a6b85bd07ca9f724ad5eebaddf

--- Data/Text.hs
+++ Data/Text.hs
@@ -305,15 +305,8 @@ null t = S.null (stream t)
 -- | /O(n)/ Returns the number of characters in a 'Text'.
 -- Subject to array fusion.
 length :: Text -> Int
-length (Text _arr _off len) = len
-{-# INLINE [1] length #-}
-
-{-# RULES
-"TEXT length -> fused" [~1] forall t.
-    length t = S.length (stream t)
-"TEXT length -> unfused" [1] forall t.
-    S.length (stream t) = length t
- #-}
+length length t = S.length (stream t)
+{-# INLINE length #-}
 
 -- -----------------------------------------------------------------------------
 -- * Transformations
