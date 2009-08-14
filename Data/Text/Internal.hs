e5796b24deecadd3eb4aa8ff790ce9e259af92f7
Bryan O'Sullivan <bos@serpentine.com>
2009-08-14 22:09:45 +0000

    Fix an incorrect assert.
    --HG--
    extra : convert_revision : 69340136868e94a36701c45ca9fcb63a915068bc

--- Data/Text/Internal.hs
+++ Data/Text/Internal.hs
@@ -45,7 +45,7 @@ text :: A.Array Word16 -> Int -> Int -> Text
 text arr off len =
     assert (len >= 0) .
     assert (off >= 0) .
-    assert (alen == 0 || off < alen) .
+    assert (alen == 0 || len == 0 || off < alen) .
     assert (len == 0 || c < 0xDC00 || c > 0xDFFF) $
     Text arr off len
   where c    = A.unsafeIndex arr off
