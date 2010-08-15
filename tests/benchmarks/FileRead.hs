040aafa30d3ed278decfbf6f6fb8200ad8fb7148
Bryan O'Sullivan <bos@serpentine.com>
2010-08-15 06:19:28 +0000

    Bigger buffer -> better performance
    --HG--
    extra : convert_revision : a7b19b3b7c7dfb094391e946ee8774b483685267

--- tests/benchmarks/FileRead.hs
+++ tests/benchmarks/FileRead.hs
@@ -7,29 +7,32 @@ import qualified Data.Text.Lazy.Encoding as T
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Search as B
+import System.IO
 
-string :: FilePath -> IO ()
-string file = readFile file >>= print . length
+string :: Handle -> IO ()
+string h = hGetContents h >>= print . length
 
-text :: FilePath -> IO ()
-text file = do
-  t <- {-# SCC "T.readFile" #-} T.readFile file
+text :: Handle -> IO ()
+text h = do
+  t <- {-# SCC "T.hGetContents" #-} T.hGetContents h
   print (T.length t)
 
-textBS :: FilePath -> IO ()
-textBS file = do
-  bs <- {-# SCC "B.readFile" #-} B.readFile file
+textBS :: Handle -> IO ()
+textBS h = do
+  bs <- {-# SCC "B.hGetContents" #-} B.hGetContents h
   print . T.length . T.decodeUtf8 $ bs
 
-bytestring :: FilePath -> IO ()
-bytestring file = do
-  bs <- {-# SCC "B.readFile" #-}B.readFile file
+bytestring :: Handle -> IO ()
+bytestring h = do
+  bs <- {-# SCC "B.hGetContents" #-} B.hGetContents h
   print (B.length bs)
 
 main = do
   (name : file : _) <- getArgs
+  h <- openFile file ReadMode
+  hSetBuffering h (BlockBuffering (Just 16384))
   case name of
-    "bs" -> bytestring file
-    "string" -> string file
-    "text" -> text file
-    "textBS" -> textBS file
+    "bs" -> bytestring h
+    "string" -> string h
+    "text" -> text h
+    "textBS" -> textBS h
