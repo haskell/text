6062679d1a47335d479294cfe3680bd269e7dce2
Bryan O'Sullivan <bos@serpentine.com>
2010-08-15 20:08:57 +0000

    Remove another unnecessary test
    --HG--
    extra : convert_revision : b56fec916dde1cfa6ee96f778c4421d9c616adfb

--- Data/Text/Encoding/Utf32.hs
+++ Data/Text/Encoding/Utf32.hs
@@ -19,5 +19,5 @@ module Data.Text.Encoding.Utf32
 import Data.Word (Word32)
 
 validate    :: Word32 -> Bool
-validate x1 = (x1 >= 0x0 && x1 < 0xD800) || (x1 > 0xDFFF && x1 <= 0x10FFFF)
+validate x1 = x1 < 0xD800 || (x1 > 0xDFFF && x1 <= 0x10FFFF)
 {-# INLINE validate #-}
