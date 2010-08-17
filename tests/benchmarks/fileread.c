f8aa5302ed6a258ebcbd12bd6c4e4587e8d8e5ed
Bryan O'Sullivan <bos@serpentine.com>
2010-08-17 03:21:49 +0000

    Add strict and lazy string support to C fileread
    --HG--
    extra : convert_revision : 8a114f72ea67b3f0e9bc23ba2fbda9b6be6a8dda

--- tests/benchmarks/fileread.c
+++ tests/benchmarks/fileread.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+void lazystring(const char *name)
+{
+    FILE *ufp = fopen(name, "r");
+    const size_t bufsize = sizeof(char) * 32 * 1024;
+    char *str = malloc(bufsize);
+    long len = 0;
+    int32_t n;
+
+    do {
+	n = fread(str, sizeof(char), bufsize, ufp);
+	len += n;
+    } while (n > 0);
+
+    printf("%ld\n", len);
+}
+
 void lazytext(const char *name)
 {
     UFILE *ufp = u_fopen(name, "r", NULL, "UTF-8");
@@ -38,6 +54,24 @@ void text(const char *name)
     printf("%d\n", n);
 }
 
+void string(const char *name)
+{
+    FILE *fp = fopen(name, "r");
+    char *str;
+    long fsize;
+    int32_t n;
+
+    fseek(fp, 0, SEEK_END);
+    fsize = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    str = malloc(sizeof(*str) * fsize);
+
+    n = fread(str, sizeof(char), fsize, fp);
+
+    printf("%d\n", n);
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 3) {
@@ -45,8 +79,12 @@ int main(int argc, char **argv)
 	exit(1);
     }
 
-    if (strcmp(argv[1], "lazytext") == 0)
+    if (strcmp(argv[1], "lazystring") == 0)
+	lazystring(argv[2]);
+    else if (strcmp(argv[1], "lazytext") == 0)
 	lazytext(argv[2]);
+    else if (strcmp(argv[1], "string") == 0)
+	string(argv[2]);
     else if (strcmp(argv[1], "text") == 0)
 	text(argv[2]);
     else {
