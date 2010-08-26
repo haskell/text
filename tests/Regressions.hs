04f2b53ec4dcfd1724dd79d163ff30ac3e395b1d
Bryan O'Sullivan <bos@serpentine.com>
2010-08-26 01:05:43 +0000

    Add a test case for the Laeremans bug.
    --HG--
    extra : convert_revision : 65cb90a96826bedfdb87aecb0fb8be1f6e02b6b6

--- tests/Regressions.hs
+++ tests/Regressions.hs
@@ -1,25 +1,44 @@
 -- Regression tests for specific bugs.
 
 import Control.Exception (bracket)
+import Control.Monad (when)
 import System.Directory (removeFile)
-import System.IO (hClose, openTempFile)
+import System.IO
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as LB
+import qualified Data.Text.IO as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LE
 import qualified Test.Framework as F
 import qualified Test.Framework.Providers.HUnit as F
 
+withTempFile = bracket (openTempFile "." "crashy.txt") cleanupTemp . uncurry
+  where
+    cleanupTemp (path,h) = do
+      open <- hIsOpen h
+      when open (hClose h)
+      removeFile path
+
 -- Reported by Michael Snoyman: UTF-8 encoding a large lazy bytestring
 -- caused either a segfault or attempt to allocate a negative number
 -- of bytes.
-lazy_encode_crash =
-  bracket (openTempFile "." "crashy.txt")
-          (\(path,h) -> hClose h >> removeFile path) $
-  \(_,h) -> LB.hPut h . LE.encodeUtf8 . LT.pack . replicate 100000 $ 'a'
+lazy_encode_crash = withTempFile $ \ _ h ->
+   LB.hPut h . LE.encodeUtf8 . LT.pack . replicate 100000 $ 'a'
+
+-- Reported by Pieter Laeremans: attempting to read an incorrectly
+-- encoded file can result in a crash in the RTS (i.e. not merely an
+-- exception).
+hGetContents_crash = withTempFile $ \ path h -> do
+  B.hPut h (B.pack [0x78, 0xc4 ,0x0a])
+  hClose h
+  h' <- openFile path ReadMode
+  --hSetEncoding h' utf8
+  T.hGetContents h' >>= print
 
 tests :: F.Test
 tests = F.testGroup "crashers" [
-          F.testCase "lazy_encode_crash" lazy_encode_crash
+          F.testCase "hGetContents_crash" hGetContents_crash
+        , F.testCase "lazy_encode_crash" lazy_encode_crash
         ]
 
 main = F.defaultMain [tests]
