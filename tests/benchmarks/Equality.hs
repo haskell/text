3c31b271fb2627de54cd5d9eb0758a4baf49dd0d
Bryan O'Sullivan <bos@serpentine.com>
2010-10-19 04:30:57 +0000

    Simplify the equality benchmark
    --HG--
    extra : convert_revision : f3d921643e2f9437add35f7ff6ae27b7097e70d9

--- tests/benchmarks/Equality.hs
+++ tests/benchmarks/Equality.hs
@@ -6,21 +6,21 @@ import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
 
-text needle haystack = do
-  bs <- B.readFile haystack
-  print . length . filter (==needle) . T.lines . T.decodeUtf8 $ bs
+text haystack = do
+  ls <- (T.lines . T.decodeUtf8) `fmap` B.readFile haystack
+  print . sum . map (\needle -> length . filter (==needle) $ ls) $ take 100 ls
 
-lazyText needle haystack = do
-  bs <- BL.readFile haystack
-  print . length . filter (==needle) . TL.lines . TL.decodeUtf8 $ bs
+lazyText haystack = do
+  ls <- (TL.lines . TL.decodeUtf8) `fmap` BL.readFile haystack
+  print . sum . map (\needle -> length . filter (==needle) $ ls) $ take 100 ls
 
-string needle haystack = do
-  s <- readFile haystack
-  print . length . filter (==needle) . lines $ s
+string haystack = do
+  ls <- lines `fmap` readFile haystack
+  print . sum . map (\needle -> length . filter (==needle) $ ls) $ take 100 ls
 
 main = do
   args <- getArgs
   case args of
-    ["text",n,h] -> text (T.pack n) h
-    ["lazytext",n,h] -> lazyText (TL.pack n) h
-    ["string",n,h] -> string n h
+    ["text",h] -> text h
+    ["lazytext",h] -> lazyText h
+    ["string",h] -> string h
