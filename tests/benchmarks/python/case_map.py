4e79b4e3632a499db316c8746c8a540a8072ee85
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-06-15 21:25:03 +0200

    More idiomatic python/ruby programs

--- tests/benchmarks/python/case_map.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import utils, sys
-
-for f in sys.argv[1:]:
-    t = utils.benchmark(lambda: utils.with_utf8_file(f, lambda c: c.upper()))
-    sys.stderr.write('{0}: {1}\n'.format(f, t))
--- tests/benchmarks/ruby/case_map.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require './utils.rb'
-
-ARGV.each do |f|
-  t = benchmark { with_utf8_file(f) { |c| c.upcase } }
-  STDERR.puts "#{f}: #{t}"
-end
