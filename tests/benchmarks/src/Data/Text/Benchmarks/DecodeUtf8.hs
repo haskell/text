30a05097dedf05be360b442bc20699afc551d806
Bryan O'Sullivan <bos@serpentine.com>
2011-07-08 13:33:18 -0700

    Benchmark the performance of iconv.
    On my Mac, it takes 33ms, vs about 20ms for the Haskell code.

--- tests/benchmarks/src/Data/Text/Benchmarks/DecodeUtf8.hs
+++ tests/benchmarks/src/Data/Text/Benchmarks/DecodeUtf8.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 -- | Test decoding of UTF-8
 --
 -- Tested in this benchmark:
@@ -16,6 +18,11 @@ module Data.Text.Benchmarks.DecodeUtf8
     ( benchmark
     ) where
 
+import Foreign.C.Types (CInt, CSize)
+import Data.ByteString.Internal (ByteString(..))
+import Foreign.Ptr (Ptr, plusPtr)
+import Foreign.ForeignPtr (withForeignPtr)
+import Data.Word (Word8)
 import Criterion (Benchmark, bgroup, bench, nf)
 import qualified Codec.Binary.UTF8.Generic as U8
 import qualified Data.ByteString as B
@@ -31,6 +38,7 @@ benchmark fp = do
     lbs <- BL.readFile fp
     return $ bgroup "DecodeUtf8"
         [ bench "Strict" $ nf T.decodeUtf8 bs
+        , bench "IConv" $ iconv bs
         , bench "StrictLength" $ nf (T.length . T.decodeUtf8) bs
         , bench "StrictInitLength" $ nf (T.length . T.init . T.decodeUtf8) bs
         , bench "Lazy" $ nf TL.decodeUtf8 lbs
@@ -41,3 +49,9 @@ benchmark fp = do
         , bench "LazyStringUtf8" $ nf U8.toString lbs
         , bench "LazyStringUtf8Length" $ nf (length . U8.toString) lbs
         ]
+
+iconv :: ByteString -> IO CInt
+iconv (PS fp off len) = withForeignPtr fp $ \ptr ->
+                        time_iconv (ptr `plusPtr` off) (fromIntegral len)
+
+foreign import ccall unsafe time_iconv :: Ptr Word8 -> CSize -> IO CInt
