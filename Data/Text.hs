93bee54a0796c5bd09fc541d4adf9f26b0b99156
Bryan O'Sullivan <bos@serpentine.com>
2014-12-03 15:35:42 -0800

    D'oh\!

--- Data/Text.hs
+++ Data/Text.hs
@@ -671,10 +671,10 @@ reverse t = S.reverse (stream t)
 --
 -- This function behaves as though it was defined as follows:
 --
---@
+-- @
 -- replace needle replacement haystack =
 --   'intercalate' replacement ('splitOn' needle haystack)
---@
+-- @
 --
 -- As this suggests, each occurrence is replaced exactly once.  So if
 -- @needle@ occurs in @replacement@, that occurrence will /not/ itself
