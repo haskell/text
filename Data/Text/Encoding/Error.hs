94334cc8b1538740c482791f81def28b4567590a
Herbert Valerio Riedel <hvr@gnu.org>
2015-12-21 10:04:00 +0100

    Tighten SafeHaskell bounds to silence warnings

--- Data/Text/Encoding/Error.hs
+++ Data/Text/Encoding/Error.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE CPP, DeriveDataTypeable #-}
-#if __GLASGOW_HASKELL__ >= 702
+#if __GLASGOW_HASKELL__ >= 704
+{-# LANGUAGE Safe #-}
+#elif __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Trustworthy #-}
 #endif
 -- |
--- Data/Text/Lazy/Read.hs
+++ Data/Text/Lazy/Read.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings, CPP #-}
-#if __GLASGOW_HASKELL__ >= 702
+#if __GLASGOW_HASKELL__ >= 704
+{-# LANGUAGE Safe #-}
+#elif __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Trustworthy #-}
 #endif
 
