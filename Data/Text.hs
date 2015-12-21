haskell/text

commit 93e3705127dc9a01c5216d7462877a27d29f8f01

Add `PrintfArg` instances for `Text` (strict and lazy).

Data/Text.hs
@@ -237,6 +237,7 @@ import Data.Int (Int64)
 #if __GLASGOW_HASKELL__ >= 708
 import qualified GHC.Exts as Exts
 #endif
+import Text.Printf (PrintfArg, formatArg, formatString)
 
 -- $strict
 --
@@ -370,6 +371,9 @@ instance Data Text where
     1 -> k (z pack)
     _ -> P.error "gunfold"
   dataTypeOf _ = textDataType
+
+instance PrintfArg Text where
+  formatArg txt = formatString $ unpack txt
 
 packConstr :: Constr
 packConstr = mkConstr textDataType "pack" [] Prefix

Data/Text/Lazy.hs
@@ -237,6 +237,7 @@ import qualified GHC.Base as GHC
 import qualified GHC.Exts as Exts
 #endif
 import GHC.Prim (Addr#)
+import Text.Printf (PrintfArg, formatArg, formatString)
 
 -- $fusion
 --
@@ -371,6 +372,9 @@ instance Data Text where
     1 -> k (z pack)
     _ -> error "Data.Text.Lazy.Text.gunfold"
   dataTypeOf _   = textDataType
+
+instance PrintfArg Text where
+  formatArg txt = formatString $ unpack txt
 
 packConstr :: Constr
 packConstr = mkConstr textDataType "pack" [] Prefix

commit d75f34cf0c728bd9d3e11ff8dd08e3a276d4139b

Fix PrintfArg fallout from 93e3705127dc9a01c5
`PrintfArg` is extensible only from base-4.7 on, prior to that the
`PrintfArg` class is not extensible.

Data/Text.hs
@@ -240,7 +240,9 @@ import Data.Int (Int64)
 #if __GLASGOW_HASKELL__ >= 708
 import qualified GHC.Exts as Exts
 #endif
+#if MIN_VERSION_base(4,7,0)
 import Text.Printf (PrintfArg, formatArg, formatString)
+#endif
 
 -- $strict
 --
@@ -387,8 +389,11 @@ instance Data Text where
     _ -> P.error "gunfold"
   dataTypeOf _ = textDataType
 
+#if MIN_VERSION_base(4,7,0)
+-- | Only defined for @base-4.7.0.0@ and later
 instance PrintfArg Text where
   formatArg txt = formatString $ unpack txt
+#endif
 
 packConstr :: Constr
 packConstr = mkConstr textDataType "pack" [] Prefix

Data/Text/Lazy.hs
@@ -240,7 +240,9 @@ import qualified GHC.Base as GHC
 import qualified GHC.Exts as Exts
 #endif
 import GHC.Prim (Addr#)
+#if MIN_VERSION_base(4,7,0)
 import Text.Printf (PrintfArg, formatArg, formatString)
+#endif
 
 -- $fusion
 --
@@ -388,8 +390,11 @@ instance Data Text where
     _ -> error "Data.Text.Lazy.Text.gunfold"
   dataTypeOf _   = textDataType
 
+#if MIN_VERSION_base(4,7,0)
+-- | Only defined for @base-4.7.0.0@ and later
 instance PrintfArg Text where
   formatArg txt = formatString $ unpack txt
+#endif
 
 packConstr :: Constr
 packConstr = mkConstr textDataType "pack" [] Prefix