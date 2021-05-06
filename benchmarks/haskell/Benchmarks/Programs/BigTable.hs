9ceaa3f83c82f79c50f6f68d20fa228142813463
Bodigrim <andrew.lelechenko@gmail.com>
2021-05-06 19:32:43 +0100

    Fix test of title conversion for Georgian characters

--- benchmarks/haskell/Benchmarks/Programs/BigTable.hs
+++ benchmarks/haskell/Benchmarks/Programs/BigTable.hs
@@ -13,7 +13,7 @@ module Benchmarks.Programs.BigTable
 
 import Test.Tasty.Bench (Benchmark, bench, whnfIO)
 #if !MIN_VERSION_base(4,8,0)
-import Data.Monoid (mconcat, mempty)
+import Data.Monoid (mconcat, mempty, mappend)
 #endif
 import Data.Text.Lazy.Builder (Builder, fromText, toLazyText)
 import Data.Text.Lazy.IO (hPutStr)
