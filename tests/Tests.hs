d15d1d9f65421ded7f4d19486ef521c1db904d39
Bodigrim <andrew.lelechenko@gmail.com>
2021-05-23 14:54:40 +0100

    Do not run inspection tests in developer mode

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,5 +1,5 @@
--- | Provides a simple main function which runs all the tests
---
+{-# LANGUAGE CPP #-}
+
 module Main
     ( main
     ) where
@@ -10,14 +10,18 @@ import qualified Tests.Lift as Lift
 import qualified Tests.Properties as Properties
 import qualified Tests.Regressions as Regressions
 
+#if !defined(ASSERTS)
 import qualified Tests.Inspection.Strict as InspectionStrict
 import qualified Tests.Inspection.Lazy   as InspectionLazy
+#endif
 
 main :: IO ()
 main = defaultMain $ testGroup "All"
   [ Lift.tests
   , Properties.tests
   , Regressions.tests
+#if !defined(ASSERTS)
   , InspectionStrict.tests
   , InspectionLazy.tests
+#endif
   ]
