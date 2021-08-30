2f90469a8f2d0980131079267e50fc85771067bd
Bodigrim <andrew.lelechenko@gmail.com>
2021-08-30 22:23:59 +0100

    Add memchr to utils.c

--- cbits/utils.c
+++ cbits/utils.c
@@ -2,10 +2,18 @@
  * Copyright (c) 2021 Andrew Lelechenko <andrew.lelechenko@gmail.com>
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 
 int _hs_text_memcmp(const void *arr1, size_t off1, const void *arr2, size_t off2, size_t len)
 {
   return memcmp(arr1 + off1, arr2 + off2, len);
+}
+
+ssize_t _hs_text_memchr(const void *arr, size_t off, size_t len, uint8_t byte)
+{
+  const void *ptr = memchr(arr + off, byte, len);
+  return ptr == NULL ? -1 : ptr - (arr + off);
 }
