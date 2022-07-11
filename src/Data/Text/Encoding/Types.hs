c1544101fdaf16c3e433345380a1ad5407853214
david-sledge <sledged@gmail.com>
2022-07-10 20:23:51 -0600

    fix documentation

--- src/Data/Text/Encoding/Types.hs
+++ src/Data/Text/Encoding/Types.hs
@@ -1,23 +1,13 @@
-{-# LANGUAGE CPP, DeriveDataTypeable #-}
 {-# LANGUAGE Safe #-}
 -- |
--- Module      : Data.Text.Encoding.Error
+-- Module      : Data.Text.Encoding.Types
 -- Copyright   : (c) Bryan O'Sullivan 2009
 --
 -- License     : BSD-style
 -- Maintainer  : bos@serpentine.com
 -- Portability : GHC
 --
--- Types and functions for dealing with encoding and decoding errors
--- in Unicode text.
---
--- The standard functions for encoding and decoding text are strict,
--- which is to say that they throw exceptions on invalid input.  This
--- is often unhelpful on real world input, so alternative functions
--- exist that accept custom handlers for dealing with invalid inputs.
--- These 'OnError' handlers are normal Haskell functions.  You can use
--- one of the presupplied functions in this module, or you can write a
--- custom handler of your own.
+-- Types to indicate the result of an attempt to decode data.
 
 module Data.Text.Encoding.Types
   ( DecodeResult(..) )
