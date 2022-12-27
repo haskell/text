8af5f4421698e51defc9f16f8d5b4fbd4e786375
Andrew Martin <andrew.thaddeus@gmail.com>
2022-12-27 10:10:54 -0500

    Expose UTF-8 validation functions from internal module
    This makes it possible for users to validate that a ByteArray is
    a well formed UTF-8 sequence. This works both with and without the
    SIMDUTF flag, falling back to a pure-Haskell implementation when
    SIMDUTF is off.
    
    Pick up a dependency on data-array-byte to be able to refer to
    the lifted ByteArray type on older versions of GHC. Use data-array-byte
    dep for emulated workflow.

--- src/Data/Text/Internal/PrimCompat.hs
+++ src/Data/Text/Internal/PrimCompat.hs
@@ -13,13 +13,12 @@ module Data.Text.Internal.PrimCompat
   ) where
 
 #if MIN_VERSION_base(4,16,0)
-
-import GHC.Base
-
+import GHC.Exts (wordToWord8#,word8ToWord#,wordToWord16#,word16ToWord#,wordToWord32#,word32ToWord#)
 #else
-
 import GHC.Prim (Word#)
+#endif
 
+#if !(MIN_VERSION_base(4,16,0))
 wordToWord8#,  word8ToWord#  :: Word# -> Word#
 wordToWord16#, word16ToWord# :: Word# -> Word#
 wordToWord32#, word32ToWord# :: Word# -> Word#
@@ -33,5 +32,4 @@ wordToWord32# w = w
 {-# INLINE word16ToWord# #-}
 {-# INLINE wordToWord32# #-}
 {-# INLINE word32ToWord# #-}
-
 #endif
