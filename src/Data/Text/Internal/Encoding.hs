f86c18e0be635e144039e47464cd713c546a03bf
Bodigrim <andrew.lelechenko@gmail.com>
2023-07-10 22:01:23 +0100

    Require bytestring-0.11.5.0

--- src/Data/Text/Internal/Encoding.hs
+++ src/Data/Text/Internal/Encoding.hs
@@ -244,7 +244,8 @@ validateUtf8Chunk bs = validateUtf8ChunkFrom 0 bs (,)
 {-# INLINE validateUtf8ChunkFrom #-}
 validateUtf8ChunkFrom :: forall r. Int -> ByteString -> (Int -> Maybe Utf8State -> r) -> r
 validateUtf8ChunkFrom ofs bs k
-#if defined(SIMDUTF) || MIN_VERSION_bytestring(0,11,2)
+  -- B.isValidUtf8 is buggy before bytestring-0.11.5.0
+#if defined(SIMDUTF) || MIN_VERSION_bytestring(0,11,5)
   | guessUtf8Boundary > 0 &&
     -- the rest of the bytestring is valid utf-8 up to the boundary
     (
