2e4933b861ad105e57132dc1c6a278fb31ab02ff
Bodigrim <andrew.lelechenko@gmail.com>
2025-04-21 16:04:53 +0100

    Regenerate case mappings

--- scripts/regenerate.sh
+++ scripts/regenerate.sh
@@ -2,6 +2,6 @@
 set -eu
 cd "${BASH_SOURCE%/*}/"
 wget http://www.unicode.org/Public/UCD/latest/ucd/SpecialCasing.txt http://www.unicode.org/Public/UCD/latest/ucd/CaseFolding.txt
-runghc-9.2 CaseMapping
+runghc-9.12 CaseMapping
 rm SpecialCasing.txt CaseFolding.txt
 cd -
